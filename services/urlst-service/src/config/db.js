2f16f022dc0fa862e342cf8fc42de7912e0a18dd
MadHoodx <edwardbat00147@gmail.com>
2025-10-23 19:18:05 +0700

    fix variable name

--- services/urlst-service/src/config/db.js
+++ services/urlst-service/src/config/db.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || '127.0.0.1',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQL_DATABASE || 'shorturl',
+  host: process.env.MYSQLHOST || process.env.MYSQL_HOST || '127.0.0.1',
+  user: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || '',
+  database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'shorturl',
+  port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
