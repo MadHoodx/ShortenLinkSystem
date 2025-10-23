798819fefaa60c229e930e4d6b260d63bcb00e5d
MadHoodx <edwardbat00147@gmail.com>
2025-10-23 20:09:48 +0700

    fix variable name

--- services/analytics-service/src/config/db.js
+++ services/analytics-service/src/config/db.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || '127.0.0.1',
-  port: process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 3306,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQL_DATABASE || 'analytics',
+  host: process.env.MYSQLHOST || process.env.MYSQL_HOST || '127.0.0.1',
+  port: process.env.MYSQLPORT || process.env.MYSQL_PORT ? Number(process.env.MYSQLPORT || process.env.MYSQL_PORT) : 3306,
+  user: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || '',
+  database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'analytics',
   waitForConnections: true,
   connectionLimit: 10,
   timezone: '+00:00' 
